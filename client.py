11ae1aa056a23b2569cd9363e99f7ce42c079101
ercchy <erika@serox.si>
2012-12-04 14:16:14 +0100

    super basic server implemented

--- client.py
+++ client.py
@@ -8,81 +8,46 @@ author: The Ercchynator :)
 import socket
 import sys
 
-class Connection(object):
-	
-	def __init__(self, host, port):	
-		super(Connection, self).__init__()
-		self.host = host
-		self.port = int(port)
+# create an AF_INER, STREAM socket (TCP)
+#Adress family: AF_INET (IPv4)
+#Type: SOCK_STREAM (TCP protocol)
 
 
-def create_socket():
-	# create an AF_INER, STREAM socket (TCP)
-	#Adress family: AF_INET (IPv4)
-	#Type: SOCK_STREAM (TCP protocol)
-	try:
-	    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	except socket.error, msg:
-	    print 'Failed to create socket. Error code: ' + str(msg[0]) + ', Error message: ' + msg[1]
-	    sys.exit()
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error, msg:
+    print 'Failed to create socket. Error code: ' + str(msg[0]) + ', Error message: ' + msg[1]
+    sys.exit()
 
-	print 'Socket created'
-	return s
+print 'Socket created'
 
 
-def get_host_by_name(s, host):	
-	try:
-	    return socket.gethostbyname(host)
-	except socket.gaierror:
-	    print 'Hostname could not be resolved, Exiting...'
-	    sys.exit()
+host = 'www.google.com'
+port = 80
 
+try:
+    remote_ip = socket.gethostbyname(host)
+except socket.gaierror:
+    print 'Hostname could not be resolved, Exiting...'
+    sys.exit()
 
-def connect_to_host(conn):
-	socket = create_socket()
+print 'IP address of ' + host + ' is ' + remote_ip
 
-	remote_ip = get_host_by_name(socket, conn.host)
-	print 'IP address of ' + conn.host + ' is ' + remote_ip
-	
-	socket.connect((remote_ip, conn.port))
+s.connect((remote_ip, port))
 
-	print 'Socket Connected to ' + conn.host + ' on IP ' + remote_ip
-	return socket
+print 'Socket Connected to ' + host + ' on IP ' + remote_ip
 
+message = 'GET / HTTP/1.1\r\n\r\n'
 
-def send_data(s, msg):
-	try:
-		s.sendall(msg)
-	except socket.error:
-		print 
-		sys.exit(1)	
-	print 'Message sent successfully'
+try:
+    s.sendall(message)
+except socket.error:
+    print 'Sending failed. Exiting ...'
+    sys.exit()
+print 'Message sent successfully'
 
+reply = s.recv(4096)
 
-def recive_data(s):
-	return s.recv(4096)
+print reply
 
-
-def main():
-	if len(sys.argv) == 2:
-		try:
-			args = sys.argv[1].split(':')
-			conn = Connection(args[0],  args[1])
-		except Exception, e:
-			print 'usage: ./client.py hostname:port'
-			sys.exit(1)		
-	elif len(sys.argv) == 1:
-		conn = Connection('localhost', 8000)
-
-	s = connect_to_host(conn)
-
-	request = 'GET / HTTP/1.1\r\n\r\n'
-
-	send_data(s, request)
-
-	print recive_data(s)
-
-	s.close()
-
-if __name__ == '__main__':
-  main()
+s.close()
---
+++ server.py
@@ -0,0 +1,81 @@
+"""
+Client - Server application creatued using sockets lib
+
+This is a Server side
+
+author: The Ercchynator :)
+"""
+import socket
+import sys
+
+class HostData(object):
+	
+	def __init__(self, port):	
+		super(HostData, self).__init__()
+		self.host = ''
+		self.port = int(port)
+
+
+def create_socket():
+	# create an AF_INER, STREAM socket (TCP)
+	# Adress family: AF_INET (IPv4)
+	# Type: SOCK_STREAM (TCP protocol)
+	try:
+	    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	except socket.error, msg:
+	    print 'Failed to create socket. Error code: ' + str(msg[0]) + ', Error message: ' + msg[1]
+	    sys.exit()
+
+	print 'Socket created'
+	return s
+
+
+def bind_socket(s, host_data):
+
+	try:
+		s.bind((host_data.host, host_data.port))
+		print 'Socket bind complete'
+		return s
+	except socket.error , msg:
+		print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message: ' + msg[1]
+        sys.exit()
+
+	
+
+
+
+def main():
+	if len(sys.argv) == 2:
+		try:
+			args = sys.argv[1].split(':')			
+			host_data = HostData(args[1])
+		except Exception, e:
+			print 'usage: ./client.py :port'
+			sys.exit(1)		
+	elif len(sys.argv) == 1:
+		host_data = HostData(8000)
+
+	s = bind_socket(create_socket(), host_data)
+
+	s.listen(10)
+	print 'Socket is now listening'
+
+	while True:
+		conn, addr = s.accept()
+		print 'Content with ' + addr[0] + ': ' + str(addr[1])
+
+
+		data = conn.recv(1024)
+		reply = 'OK...' + data
+		if not data:
+			break
+
+		conn.sendall(reply)
+
+	conn.close()
+	s.close()
+
+
+
+if __name__ == '__main__':
+  main()
