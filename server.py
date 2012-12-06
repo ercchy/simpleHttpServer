e0d44f4d8c462d6ea7dafd913ea99ca6ab9646b4
ercchy <erika@serox.si>
2012-12-06 09:58:01 +0100

    implementing threadpool

--- server.py
+++ server.py
@@ -7,30 +7,11 @@ author: The Ercchynator :)
 """
 import socket
 import sys
-from thread import *
+from Queue import Queue
+from threading import Thread
+from SimpleHTTPServer import SimpleHTTPRequestHandler
 
-class HostData(object):
-	
-	def __init__(self, port):	
-		super(HostData, self).__init__()
-		self.host = ''
-		self.port = int(port)
-
-
-def create_socket():
-	# create an AF_INER, STREAM socket (TCP)
-	# Adress family: AF_INET (IPv4)
-	# Type: SOCK_STREAM (TCP protocol)
-	try:
-	    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	except socket.error, msg:
-	    print 'Failed to create socket. Error code: ' + str(msg[0]) + ', Error message: ' + msg[1]
-	    sys.exit()
-
-	print 'Socket created'
-	return s
-
-
+"""
 def bind_socket(s, host_data):
 
 	try:
@@ -38,48 +19,199 @@ def bind_socket(s, host_data):
 		print 'Socket bind complete'
 		return s
 	except socket.error , msg:
+		s.close()
 		print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message: ' + msg[1]
         sys.exit()
 
 
 def open_thread(conn):
-	conn.send('Welocme to the server. Type something and then hit enter\n')
-
+	
+	data = None
 	while True:
-		data = conn.recv(1024)
-		reply = 'OK...' + data
+		try:
+			data = conn.recv(1024)
+			reply = 'HTTP/1.0 200 OK\r\nContent-Type:text/html\r\nConnection:keep-alive\r\n\r\n<html><head>foo</head></html>\r\n'
+			print data
+		except Error as e:
+			print 'Connection is not working dammit' + e
+
+		
 		if not data:
 			break
 
 		conn.sendall(reply)
+
 	conn.close()
 
+"""
+class BaseServer:
+    # create an AF_INER, STREAM socket (TCP)
+    # Adress family: AF_INET (IPv4)
+    # Type: SOCK_STREAM (TCP protocol)
+    adress_family = socket.AF_INET
+
+    socket_type = socket.SOCK_STREAM
+
+    request_queue_size = 10
+
+    def __init__(self, server_address, bind_and_activate=True):
+        self.socket = socket.socket(self.adress_family, self.socket_type)
+        self.server_address = server_address
+
+        if bind_and_activate:
+            self.server_bind()
+            self.server_activate()
 
 
-def main():
-	if len(sys.argv) == 2:
-		try:
-			args = sys.argv[1].split(':')			
-			host_data = HostData(args[1])
-		except Exception, e:
-			print 'usage: ./client.py :port'
-			sys.exit(1)		
-	elif len(sys.argv) == 1:
-		host_data = HostData(8000)
+    def server_bind(self):
+        self.socket.bind(self.server_address)
+        self.server_address = self.socket.getsockname()
 
+
+    def server_activate(self):
+        self.socket.listen(self.request_queue_size)
+
+
+    def server_close(self):
+        self.socket.close()
+
+
+    def serve_forever(self, threadpool):
+        while True:
+            conn, addr = self.socket.accept()
+            print 'Connected with ' + addr[0] + ':' + str(addr[1])
+
+
+
+
+class Worker(Thread):
+    """Thread executing tasks from a given tasks queue"""
+    def __init__(self, tasks):
+        Thread.__init__(self)
+        self.tasks = tasks
+        self.daemon = True
+        self.start()
+
+    def run(self):
+        while True:
+            func, args, kargs = self.tasks.get()
+            try: func(*args, **kargs)
+            except Exception, e: print e
+            self.tasks.task_done()
+
+
+class ThreadPool:
+    """Pool of threads consuming tasks from a queue"""
+    def __init__(self, num_threads):
+        self.tasks = Queue(num_threads)
+        for _ in range(num_threads): Worker(self.tasks)
+
+    def add_task(self, func, *args, **kargs):
+        """Add a task to the queue"""
+        self.tasks.put((func, args, kargs))
+
+    def wait_completion(self):
+        """Wait for completion of all the tasks in the queue"""
+        self.tasks.join()
+
+
+class BasicHTTPThreadingServer(ThreadPool, BaseServer):
+
+    def server_bind(self):
+        BaseServer.server_bind(self)
+
+        host, port = self.socket.getsockname()[:2]
+        print host
+        self.server_name = socket.getfqdn(host)
+        self.server_port = port
+        self.pool = ThreadPool(20)
+
+
+    def serve_forever(self, threadpool):
+        while True:
+            conn, addr = self.socket.accept()
+            print 'Connected with ' + addr[0] + ':' + str(addr[1])
+            #self.pool.add_task(conn)
+
+
+        """
+        try:
+            s.bind((host_data.host, host_data.port))
+            print 'Socket bind complete'
+            return s
+        except socket.error , msg:
+            s.close()
+            print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message: ' + msg[1]
+        sys.exit()
+        """
+
+
+
+def test():
+    if sys.argv[1:]:
+        port = sys.argv[1]
+    else:
+        port = 8000
+
+    server_address = ('', port)
+    try:
+        httpd = BasicHTTPThreadingServer(server_address)
+        sa = httpd.socket.getsockname()
+        print "Serving HTTP on", sa[0], "port", sa[1], "..."
+        #httpd.serve_forever(httpd.pool)
+    except KeyboardInterrupt:
+        httpd.server_close()
+
+
+
+    """
 	s = bind_socket(create_socket(), host_data)
 
 	s.listen(10)
 	print 'Socket is now listening'
-
+    """
+    """
 	while True:
 		conn, addr = s.accept()
 		print 'Content with ' + addr[0] + ': ' + str(addr[1])
 		start_new_thread(open_thread,(conn,))
 	
 	s.close()
-
+    """
 
 
 if __name__ == '__main__':
-  main()
+    test()
+
+"""
+import SocketServer
+
+class BaseHTTPMultiThreadServer(SocketServer.ThreadingTCPServer):
+
+    def server_bind(self):
+        SocketServer.ThreadingTCPServer.server_bind(self)
+        host, port = self.socket.getsockname()[:2]
+        self.server_name = socket.getfqdn(host)
+        self.server_port = port
+
+
+class BaseRequestHandler:
+    pass
+
+
+def test():
+    if sys.argv[1:]:
+        port = int(sys.argv[1])
+    else:
+        port = 8000
+    server_address = ('', port)
+
+    httpd = BaseHTTPMultiThreadServer(server_address, BaseRequestHandler)
+    sa = httpd.socket.getsockname()
+
+    print "Serving HTTP on", sa[0], "port", sa[1], "..."
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    test()
+"""
